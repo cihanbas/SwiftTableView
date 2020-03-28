9abb24e7498f130f7e210eddddb52cce0740e1da
cihan_bas <Varidat65>
2020-03-28 23:30:59 +0300

    Initial Commit

---
+++ tableViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewTest
+//
+//  Created by cihanbas on 28.03.2020.
+//  Copyright © 2020 cihanbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
